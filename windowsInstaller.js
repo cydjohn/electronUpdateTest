9b9c3306f92a4530d7daaa66d93f9f4e5a98ff68
cydjohn <cydjohn@live.com>
2018-06-17 16:40:28 -0400

    update

--- windowsInstaller.js
+++ windowsInstaller.js
@@ -2,13 +2,12 @@ var electronInstaller = require('electron-winstaller');
 const path = require('path')
 resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: './out/MyApp-win32-ia32',
+    //appDirectory: './dist/win-unpacked',
     outputDirectory: './windows32',
     authors: 'cyd',
     exe: 'MyApp.exe',
-
     outputDirectory: path.join('./out/', 'windows-installer'),
-    setupExe: 'ElectronAPIDemosSetup.exe',
-
+    setupExe: 'ElectronAPIDemosSetup.exe'
   });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
